7de3e05f3bbe4c14504a2672e4791d60eb48ec14
daian <daian@DESKTOP-A982T2R>
2021-03-04 15:28:54 +0200

    shortcuts

--- CTS_seminar1/src/ro/ase/csie/cts/seminar2/Main.java
+++ CTS_seminar1/src/ro/ase/csie/cts/seminar2/Main.java
@@ -61,7 +61,13 @@ public class Main {
 //			System.out.println(e.getMessage());
 //		}
 		
-
+		
+		//SHORTCUTS
+		
+		//syso(+ctrl si spacebar + enter) -> shortcut pt System.out.println
+		//sau ctrl+1 pt altele
+		//ctrl+o gasire mai rapida metoda
+		//ctrl+shift+t sa gasim clasa/exceptia etc in pachete
 	}
 
 }
