494c8b8f8a0addb4a07ba91d43fdad58d69ec942
daian <daiana.tabirca@gmail.com>
2021-04-14 14:41:05 +0300

    Adapter

---
+++ src/ro/ase/csie/cts/g1087/dp/adapter/ActiuniDisney.java
@@ -0,0 +1,9 @@
+package ro.ase.csie.cts.g1087.dp.adapter;
+
+public interface ActiuniDisney {
+	
+	public void move(int x, int y);
+	public void primesteLovitura(int power);
+	public void reincarca(int power);
+	public void jump();
+}
