6e988c456ec5d75a484401363ba8e23338b7e5be
daian <daiana.tabirca@gmail.com>
2021-05-12 22:05:03 +0300

    refacere Singleton Main

--- src/ro/ase/csie/cts/g1087/dp/singleton/TestSingleton.java
+++ src/ro/ase/csie/cts/g1087/dp/singleton/TestSingleton.java
@@ -5,17 +5,16 @@ public class TestSingleton {
 	public static void main(String[] args) {
 		ConexiuneBD conexiune1 = ConexiuneBD.getConexiune();
 		ConexiuneBD conexiune2 = ConexiuneBD.getConexiune();
-		
-		if(conexiune1 == conexiune2) {
-			System.out.println("Cele 2 referinte sunt identice");
-		}
-		else
-		{
-			System.out.println("Cele 2 referinte sunt diferite");		}
 
+		if (conexiune1 == conexiune2) {
+			System.out.println("Cele 2 referinte sunt identice"); //referintele vor fi identice
+		} else {
+			System.out.println("Cele 2 referinte sunt diferite"); 
+		}
+
+		ModulUI modulUI = new ModulUI();
+		ModulProfil modulProfil = new ModulProfil();
+			
 	}
-	
-	ModulUI modulUI = new ModulUI();
-	ModulProfil modulProfil = new ModulProfil();
 
 }
