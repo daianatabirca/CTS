60b88267ec3be2779510143939c5748d65123761
daian <daiana.tabirca@gmail.com>
2021-06-04 23:10:12 +0300

    completari test memento

---
+++ src/ro/ase/csie/cts/g1087/dp/memento/TestMemento.java
@@ -0,0 +1,20 @@
+package ro.ase.csie.cts.g1087.dp.memento;
+
+public class TestMemento {
+
+	public static void main(String[] args) {
+
+		CaracterJoc superman = new CaracterJoc("Superman", 100, "zboara");
+
+		MementoCaracterJoc salvareInceputJoc = superman.salvare();
+
+		superman.ataca();
+		superman.seVindeca(100);
+		System.out.println("Puncte viata: " + superman.puncteViata);
+
+		superman.incarcaSalvare(salvareInceputJoc);
+
+		System.out.println("Puncte viata: " + superman.puncteViata);
+	}
+
+}
